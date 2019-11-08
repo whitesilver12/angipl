bd49afca85e90f45a375ae2fc21338fd934b19bd
John Paolo <paolo716@gmail.com>
2019-11-08 12:55:03 +0800

    Install shoulder-matchers gem, create word spec

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   gem 'chromedriver-helper'
+  gem 'shoulda-matchers', '~> 4.1', '>= 4.1.2'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "columns" do
+    it { is_expected.to have_db_column(:value)}
+    it { is_expected.to have_db_column(:language)}
+  end
+  
 end
